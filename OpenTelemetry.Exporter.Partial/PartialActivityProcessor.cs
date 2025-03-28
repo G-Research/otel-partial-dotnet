9582f05d25ec5760a2c3bf2d4b343417e5d8c289
Mladjan Gadzic <gadzic.mladjan@gmail.com>
2025-03-28 18:53:44 +0100

    access otel internals in constructor to fail fast if error happens instead of waiting for the first heartbeat

--- OpenTelemetry.Exporter.Partial/PartialActivityProcessor.cs
+++ OpenTelemetry.Exporter.Partial/PartialActivityProcessor.cs
@@ -1,5 +1,6 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Reflection;
 using OpenTelemetry.Logs;
 
 namespace OpenTelemetry.Exporter.Partial;
@@ -16,6 +17,10 @@ public class PartialActivityProcessor : BaseProcessor<Activity>
     private ConcurrentQueue<KeyValuePair<ActivitySpanId, Activity>> endedActivities;
     private BaseExporter<LogRecord> logExporter;
 
+    private static MethodInfo WriteTraceDataMethod;
+    private static ConstructorInfo LogRecordConstructor;
+    private static object SdkLimitOptions;
+
     public PartialActivityProcessor(
         BaseExporter<LogRecord> logExporter,
         int scheduledDelayMilliseconds = DefaultScheduledDelayMilliseconds)
@@ -31,6 +36,10 @@ public class PartialActivityProcessor : BaseProcessor<Activity>
 
         exportTrigger = new AutoResetEvent(false);
         shutdownTrigger = new ManualResetEvent(false);
+
+        // Access OpenTelemetry internals as soon as possible to fail fast rather than waiting for the first heartbeat
+        AccessOpenTelemetryInternals(out WriteTraceDataMethod, out LogRecordConstructor,
+            out SdkLimitOptions);
 
         exporterThread = new Thread(ExporterProc)
         {
@@ -86,66 +95,13 @@ public class PartialActivityProcessor : BaseProcessor<Activity>
         List<KeyValuePair<string, object?>> logRecordAttributesToBeAdded)
     {
         var buffer = new byte[750000];
-        var sdkLimitOptionsType = Type.GetType(
-            "OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation.SdkLimitOptions, OpenTelemetry.Exporter.OpenTelemetryProtocol",
-            true);
 
-        if (sdkLimitOptionsType == null)
-        {
-            throw new InvalidOperationException("Failed to get the type 'SdkLimitOptions'.");
-        }
-
-        var sdkLimitOptions = Activator.CreateInstance(sdkLimitOptionsType, nonPublic: true);
-
-        if (sdkLimitOptions == null)
-        {
-            throw new InvalidOperationException(
-                "Failed to create an instance of 'SdkLimitOptions'.");
-        }
-
-        var protobufOtlpTraceSerializerType = Type.GetType(
-            "OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation.Serializer.ProtobufOtlpTraceSerializer, OpenTelemetry.Exporter.OpenTelemetryProtocol",
-            true);
-
-        if (protobufOtlpTraceSerializerType == null)
-        {
-            throw new InvalidOperationException(
-                "Failed to get the type 'ProtobufOtlpTraceSerializer'.");
-        }
-
-        var writeTraceDataMethod = protobufOtlpTraceSerializerType.GetMethod(
-            "WriteTraceData",
-            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
-
-        if (writeTraceDataMethod == null)
-        {
-            throw new InvalidOperationException("Failed to get the method 'WriteTraceData'.");
-        }
-
-        var result = writeTraceDataMethod.Invoke(
+        var result = WriteTraceDataMethod.Invoke(
             null,
-            [buffer, 0, sdkLimitOptions, null!, new Batch<Activity>(data)]);
+            [buffer, 0, SdkLimitOptions, null!, new Batch<Activity>(data)]);
         var writePosition = result as int? ?? 0; // Use a default value if null
 
-        var logRecordType = Type.GetType("OpenTelemetry.Logs.LogRecord, OpenTelemetry", true);
-
-        if (logRecordType == null)
-        {
-            throw new InvalidOperationException("Failed to get the type 'LogRecord'.");
-        }
-
-        var logRecordConstructor = logRecordType.GetConstructor(
-            System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
-            null,
-            Type.EmptyTypes,
-            null);
-
-        if (logRecordConstructor == null)
-        {
-            throw new InvalidOperationException("Failed to get the constructor of 'LogRecord'.");
-        }
-
-        var logRecord = (LogRecord)logRecordConstructor.Invoke(null);
+        var logRecord = (LogRecord)LogRecordConstructor.Invoke(null);
         logRecord.Timestamp = DateTime.UtcNow;
         logRecord.TraceId = data.TraceId;
         logRecord.SpanId = data.SpanId;
@@ -160,6 +116,56 @@ public class PartialActivityProcessor : BaseProcessor<Activity>
         logRecord.Attributes = logRecordAttributes;
 
         return logRecord;
+    }
+
+    private static void AccessOpenTelemetryInternals(out MethodInfo writeTraceDataMethod,
+        out ConstructorInfo logRecordConstructor, out object sdkLimitOptions)
+    {
+        var sdkLimitOptionsType = Type.GetType(
+            "OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation.SdkLimitOptions, OpenTelemetry.Exporter.OpenTelemetryProtocol",
+            true);
+
+        if (sdkLimitOptionsType == null)
+        {
+            throw new InvalidOperationException("Failed to get the type 'SdkLimitOptions'.");
+        }
+
+        sdkLimitOptions = Activator.CreateInstance(sdkLimitOptionsType, nonPublic: true) ??
+                          throw new InvalidOperationException(
+                              "Failed to create an instance of 'SdkLimitOptions'.");
+
+        var protobufOtlpTraceSerializerType = Type.GetType(
+            "OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation.Serializer.ProtobufOtlpTraceSerializer, OpenTelemetry.Exporter.OpenTelemetryProtocol",
+            true);
+
+        if (protobufOtlpTraceSerializerType == null)
+        {
+            throw new InvalidOperationException(
+                "Failed to get the type 'ProtobufOtlpTraceSerializer'.");
+        }
+
+        writeTraceDataMethod =
+            protobufOtlpTraceSerializerType.GetMethod("WriteTraceData",
+                BindingFlags.NonPublic | BindingFlags.Static) ??
+            throw new InvalidOperationException("Failed to get the method 'WriteTraceData'.");
+
+        if (writeTraceDataMethod == null)
+        {
+            throw new InvalidOperationException("Failed to get the method 'WriteTraceData'.");
+        }
+
+        var logRecordType = Type.GetType("OpenTelemetry.Logs.LogRecord, OpenTelemetry", true);
+
+        if (logRecordType == null)
+        {
+            throw new InvalidOperationException("Failed to get the type 'LogRecord'.");
+        }
+
+        logRecordConstructor = logRecordType.GetConstructor(
+                                   BindingFlags.NonPublic | BindingFlags.Instance, null,
+                                   Type.EmptyTypes, null) ??
+                               throw new InvalidOperationException(
+                                   "Failed to get the constructor of 'LogRecord'.");
     }
 
     public override void OnEnd(Activity data)
